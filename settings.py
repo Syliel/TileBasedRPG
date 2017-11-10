d953645314d7edba61ef675968c8ac963ee384bf
Jenni Bell <jbellcollins@gmail.com>
2017-11-09 18:04:43 -0600

    What

---
+++ settings.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from UltraColor import *
+
+WIDTH = 1024
+HEIGHT = 768
+FPS = 60
+TITLE = "Tilemap Demo"
+BGCOLOR = Color.Gray
+SPRITESHEET = "mousie1.png"
+
+TILESIZE = 16
+GRIDWIDTH = WIDTH / TILESIZE
+GRIDHEIGHT = HEIGHT / TILESIZE
+
+#Player Settings
+PLAYER_SPEED = 300
+PLAYER_IMG = 'mousie1.png'
---
+++ sprites.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+import pygame as pg
+from settings import *
+from UltraColor import *
+import pytmx
+vec = pg.math.Vector2
+
+
+class Spritesheet:
+    # utility class for loading and parsing spritesheets
+    def __init__(self, filename):
+        self.spritesheet = pg.image.load(filename).convert()
+
+    def get_image(self, x, y, width, height):
+        # grab an image out of a larger spritesheet
+        image = pg.Surface((width, height))
+        image.blit(self.spritesheet, (0, 0), (x, y, width, height))
+        return image
+
+
+class Player(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.get_faces()
+        self.image = self.south
+        self.image.set_colorkey(Color.Black)
+        self.rect = self.image.get_rect()
+        self.vel = vec(0, 0)
+        self.pos = vec(x, y) * TILESIZE
+
+    def get_faces(self):
+        self.south = self.game.spritesheet.get_image(0, 0, 32, 32)
+        self.south.set_colorkey(Color.Black)
+        self.north = self.game.spritesheet.get_image(32, 32, 32, 32)
+        self.north.set_colorkey(Color.Black)
+        self.east = self.game.spritesheet.get_image(0, 32, 32, 32)
+        self.east.set_colorkey(Color.Black)
+        self.west = self.game.spritesheet.get_image(32, 0, 32, 32)
+        self.west.set_colorkey(Color.Black)
+
+
+    def get_keys(self):
+        self.vel = vec(0, 0)
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT] or keys[pg.K_a]:
+            self.vel.x = -PLAYER_SPEED
+            self.image = self.west
+        if keys[pg.K_RIGHT] or keys[pg.K_d]:
+            self.vel.x = PLAYER_SPEED
+            self.image = self.east
+        if keys[pg.K_UP] or keys[pg.K_w]:
+            self.vel.y = -PLAYER_SPEED
+            self.image = self.north
+        if keys[pg.K_DOWN] or keys[pg.K_s]:
+            self.vel.y = PLAYER_SPEED
+            self.image = self.south
+        if self.vel.x != 0 and self.vel.y != 0:
+            self.vel *= 0.7071
+
+    def collide_with_walls(self, dir):
+        if dir == 'x':
+            hits = pg.sprite.spritecollide(self, self.game.walls, False)
+            if hits:
+                if self.vel.x > 0:
+                    self.pos.x = hits[0].rect.left - self.rect.width
+                if self.vel.x < 0:
+                    self.pos.x = hits[0].rect.right
+                self.vel.x = 0
+                self.rect.x = self.pos.x
+        if dir == 'y':
+            hits = pg.sprite.spritecollide(self, self.game.walls, False)
+            if hits:
+                if self.vel.y > 0:
+                    self.pos.y = hits[0].rect.top - self.rect.height
+                if self.vel.y < 0:
+                    self.pos.y = hits[0].rect.bottom
+                self.vel.y = 0
+                self.rect.y = self.pos.y
+
+    def update(self):
+        self.get_keys()
+        self.pos += self.vel * self.game.dt
+        self.rect.x = self.pos.x
+        self.collide_with_walls('x')
+        self.rect.y = self.pos.y
+        self.collide_with_walls('y')
+
+class Wall(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.walls
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(Color.Green)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
+
+
+
+
+
